fa2e6aa2416e4bb8e9f782d4ac2b234abdc1b902
dailylicense76-ship-it <dailylicense76@gmail.com>
2026-05-21 16:48:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,13 @@
-const CACHE_NAME = 'degz-pulse-v35-4';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
-  'https://cdn.jsdelivr.net/npm/chart.js',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+    console.log('[Degz Pulse] Service Worker Installed');
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
+self.addEventListener('activate', (e) => {
+    console.log('[Degz Pulse] Service Worker Activated');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => caches.match('./index.html'));
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    // Basic fetch passthrough para tuloy-tuloy ang connection sa API
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
