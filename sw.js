e2f65a222ce0dff376541c52298e4209feafc161
dailylicense76-ship-it <dailylicense76@gmail.com>
2026-05-21 17:02:03 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,30 @@
-self.addEventListener('install', (e) => {
-    console.log('[Degz Pulse] Service Worker Installed');
+const CACHE_NAME = 'degz-pulse-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('[Degz Pulse] Caching offline files');
+            return cache.addAll(urlsToCache);
+        })
+    );
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
     console.log('[Degz Pulse] Service Worker Activated');
 });
 
-self.addEventListener('fetch', (e) => {
-    // Basic fetch passthrough para tuloy-tuloy ang connection sa API
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
